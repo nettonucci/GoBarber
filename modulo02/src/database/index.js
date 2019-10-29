02f9612408a3762d9a65f449c0834be55837ade5
Netto <nettonucci@gmail.com>
2019-10-28 22:36:59 -0300

    Migration e model de agendamento.

--- modulo02/src/database/index.js
+++ modulo02/src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from "sequelize";
 
 import User from "../app/models/User";
 import File from "../app/models/file";
+import Appointment from "../app/models/Appointment";
 
 import databaseConfig from "../Config/database";
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
